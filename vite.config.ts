9fbc05f7d48963039667e08923303dce79f8bda9
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-17 13:12:56 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
   root: "client",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client/src"),
