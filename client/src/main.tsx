95ee6c2ba82cd19d3aaac0fc6c098e035d8199ec
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-17 15:04:48 -0700

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,13 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
-// Get the root element from the HTML
-const container = document.getElementById("root");
-if (!container) {
-  throw new Error("Root container missing in index.html");
-}
-
-// Create a root and render the App component
-const root = createRoot(container);
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
