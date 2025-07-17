f363cc5fb71f32c825fa5545d12f60c617d72e0d
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-17 12:31:30 -0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { queryClient } from "./lib/queryClient";
+import { queryClient } from "@/lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
