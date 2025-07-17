5e198e18d61c679759389a58cfc54b698a3d16d6
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-17 11:09:04 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,20 +8,19 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'client/src'),
-      '@assets': path.resolve(__dirname, 'client/src/assets')
-    }
+      '@assets': path.resolve(__dirname, 'client/src/assets'),
+    },
   },
   build: {
     outDir: path.resolve(__dirname, 'client/dist'),
     sourcemap: false,
     emptyOutDir: true,
     rollupOptions: {
-      external: []
-    }
+      external: [],
+    },
   },
   server: {
     port: 3000,
-    strictPort: true
-  }
+    strictPort: true,
+  },
 });
-
