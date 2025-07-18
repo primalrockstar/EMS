a8c54ff6e15cf3223a7b0112470af131f81f1485
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-17 22:52:26 -0700

    clean: remove vite.config.ts and update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,18 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import path from "path";
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  root: "client",
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "client/src"),
-    },
-  },
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-  },
+  plugins: [tsconfigPaths()],
 });
