d7fdcd27aa003412033c968e998c759c172d5a3e
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-18 22:22:39 -0700

    Update schema.ts

--- shared/schema.ts
+++ shared/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, serial, integer, boolean, timestamp, jsonb, varchar } from "drizzle-orm/pg-core";
+import { pgTable, text, serial, integer, boolean, timestamp, jsonb } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
