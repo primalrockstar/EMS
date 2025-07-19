bfed61c95e447dc49617f085a1169ccc87335640
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-19 16:10:12 -0700

    Update vite.config.mjs

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,10 +1,8 @@
-// This file is in the client folder (EMS/client/vite.config.mjs)
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-// __dirname workaround for ESM
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
