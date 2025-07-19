6a7775c11ee2dee9bd2dc66542556cd212885db7
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-19 16:09:48 -0700

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,8 @@
-// This file is at the monorepo root (EMS/vite.config.mjs)
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-// __dirname workaround for ESM
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
