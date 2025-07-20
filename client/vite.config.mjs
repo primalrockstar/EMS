6249985dab78233d14ce5ee1fce24900bbec77ee
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-19 21:42:18 -0700

    Update vite.config.mjs

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-// Needed for __dirname in ESM
+// Polyfill __dirname in ESM
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
