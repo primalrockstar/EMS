61b0ba0d7790533642803991420da270090ac643
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-20 14:29:04 -0700

    Update vite.config.mjs

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(process.cwd(), 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-})
+});
