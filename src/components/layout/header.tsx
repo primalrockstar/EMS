491755332cf98ee14b94de2614cf86a9a63addbb
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-20 13:54:05 -0700

    Update header.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,11 @@
 export default function Header() {
   return (
     <header className="h-16 flex items-center justify-center bg-white shadow">
-      <img src="/logo.png" alt="ProMedix EMS Logo" className="h-10" />
+      <img
+        src="/logo2_1752462736002.png"
+        alt="ProMedix EMS Logo"
+        className="h-10"
+      />
     </header>
   );
 }
