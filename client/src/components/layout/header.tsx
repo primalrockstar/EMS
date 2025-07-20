057b64da1cd14d0424426880926f8fe3e2cdec69
Your Name <your@email.com>
2025-07-20 14:03:49 -0700

    Show only logo in header, remove EMS app text

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="flex items-center p-4 bg-white shadow">
       <img src="/logo2_1752462736002.png" alt="Logo" className="h-10 mr-2" />
-      <span className="font-bold text-xl">EMS App</span>
+      <span className="font-bold text-xl"></span>
       {/* Add more header elements here if needed */}
     </header>
   );
