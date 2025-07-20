9aaaf31881547381e112a13de6fb237dd541b13e
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-19 22:16:32 -0700

    Update vite.config.mjs

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -2,15 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      '@shared': path.resolve(__dirname, '../shared'),  // <-- ADD THIS LINE
     },
   },
 })
