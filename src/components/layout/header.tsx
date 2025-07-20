22a7aa9f99d1218414f246b51d5cf6bb6bbf7403
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-20 13:08:15 -0700

    Update header.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="bg-gray-200 border-b p-4">
-      <h1 className="text-lg font-bold">Header</h1>
+      {/* Removed the header text */}
     </header>
   );
 }
