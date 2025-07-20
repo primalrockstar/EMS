5614d7c2648784adcd4603ac09bafdb2abccd53c
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-20 13:45:22 -0700

    Update header.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,12 +1,5 @@
 export default function Header() {
   return (
-    <header className="bg-white border-b flex items-center p-4">
-      <img
-        src="/logo2_1752462736002.png"
-        alt="ProMedixEMS Logo"
-        className="h-10 mr-2"
-      />
-      {/* Removed EMS App text */}
-    </header>
+    <header className="h-16" />
   );
 }
